87f07f05a9edac049d2a8153e5e061c11bc0990e
utop1an <vfqq1066@outlook.com>
2023-01-29 01:45:36 +1100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AutofocusFixModule } from 'ngx-autofocus-fix';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AutofocusFixModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+import { trigger, transition, style, animate } from '@angular/animations';
+import { TodoService } from 'src/app/services/todo.service';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss'],
+  providers: [TodoService],
+  animations: [
+    trigger('fade', [
+
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(30px)' }),
+        animate(200, style({ opacity: 1, transform: 'translateY(0px)'}))
+      ]),
+
+      transition(':leave', [
+        animate(200, style({ opacity: 0, transform: 'translateY(30px)' }))
+      ]),
+
+    ])
+  ]
+})
+export class TodoListComponent implements OnInit {
+  todoTitle: string;
+
+  constructor(public todoService : TodoService) { 
+  }
+
+  ngOnInit() {
+    this.todoTitle = '';
+  
+  }
+
+  addTodo(): void {
+    this.todoService.addTodo(this.todoTitle);
+    this.todoTitle = '';
+  }
+
+  
+
+}
